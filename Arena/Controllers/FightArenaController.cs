1e1b4d25ddf6c37c23db818b197ac2226e6b1803
jporwol <pako1337@gmail.com>
2014-09-30 18:21:30 +0200

    added empty fight arena controller and view

---
+++ Arena/Controllers/FightArenaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Arena.Controllers
+{
+    public class FightArenaController : Controller
+    {
+        // GET: FightArena
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
