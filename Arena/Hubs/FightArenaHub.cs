c7b80062c3022cfe3b735b5ed5d07322781a002c
jporwol <pako1337@gmail.com>
2014-10-01 20:09:00 +0200

    send list of all active users to new user on register

--- Arena/Hubs/FightArenaHub.cs
+++ Arena/Hubs/FightArenaHub.cs
@@ -3,13 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Microsoft.AspNet.SignalR;
+using System.Collections.Concurrent;
 
 namespace Arena.Hubs
 {
     public class FightArenaHub : Hub
     {
+        private static ConcurrentBag<string> _users = new ConcurrentBag<string>();
+
         public void Register()
         {
+            _users.Add(Context.ConnectionId);
+
+            _users.ToList()
+                .Where(u => u != Context.ConnectionId)
+                .Select(u => Clients.Client(Context.ConnectionId).NewUser(u))
+                .ToList();
+
             Clients.AllExcept(Context.ConnectionId).NewUser(Context.ConnectionId);
         }
 
