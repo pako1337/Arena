4831c4986a240e7aa79b197ab4350111cd355d3c
jporwol <pako1337@gmail.com>
2015-01-25 16:51:05 +0100

    display player token on canvas

--- Arena/Scripts/FightArena/Arena.js
+++ Arena/Scripts/FightArena/Arena.js
@@ -1,30 +1,38 @@
 ﻿define("Arena", function () {
     function Arena(arenaDispaly) {
-        self = this;
-        self.arena = arenaDispaly.getContext("2d");
+        this.arena = arenaDispaly.getContext("2d");
+        this.size = { x: arenaDispaly.width, y: arenaDispaly.height };
+        this.players = [];
+
+        var self = this;
+        var tick = function () {
+            self.update();
+            requestAnimationFrame(tick);
+        }
 
         this.addPlayer = function (players) {
             for (var i = 0; i < players.length; i++) {
-                createElement(players[i]);
+                self.players.push(players[i]);
             }
         };
 
         this.removePlayer = function (id) {
-            var playerToken = document.getElementById(id);
-            if (playerToken === null) return;
-            arena.removeChild(playerToken);
         };
 
         this.updatePlayer = function (id, player) {
-            var playerToken = document.getElementById(id);
-            if (playerToken == undefined || playerToken == null) return;
-            setPosition(playerToken, player.Position);
         };
 
-        var createElement = function (player) {
-            self.arena.fillRect(player.Position.X, player.Position.Y, 50, 50);            
-        };
+        tick();
     };
+
+    Arena.prototype = {
+        update: function() {
+            this.arena.clearRect(0, 0, this.size.x, this.size.y);
+            for (var i = 0; i < this.players.length; i++) {
+                this.arena.fillRect(this.players[i].Position.X, this.players[i].Position.Y, 50, 50);
+            }
+        },
+    }
 
     return Arena;
 });
