b68d2720584397d4416a1bcd2026ac64e125d69b
jporwol <pako1337@gmail.com>
2015-02-04 21:24:59 +0100

    passing enum status as string to UI

--- ArenaModel/Player.cs
+++ ArenaModel/Player.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 
 namespace ArenaModel
 {
     public class Player
     {
+        [JsonConverter(typeof(StringEnumConverter))]
         public PlayerStatus Status { get; set; }
         public ArenaObject PlayerToken { get; set; }
 
