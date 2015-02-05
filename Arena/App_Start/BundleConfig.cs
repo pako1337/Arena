05d538ed4cd31ff02cc68522823f3df47e197d9c
jporwol <pako1337@gmail.com>
2015-02-05 20:14:23 +0100

    disable boundling for now

--- Arena/App_Start/BundleConfig.cs
+++ Arena/App_Start/BundleConfig.cs
@@ -32,7 +32,7 @@ namespace ArenaUI
 
             // Set EnableOptimizations to false for debugging. For more information,
             // visit http://go.microsoft.com/fwlink/?LinkId=301862
-            BundleTable.EnableOptimizations = true;
+            BundleTable.EnableOptimizations = false;
         }
     }
 }
