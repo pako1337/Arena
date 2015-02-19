8dc9a47e722672b51387c02a55c8954315eedc0d
jporwol <pako1337@gmail.com>
2015-02-19 21:00:24 +0100

    moved drawing logic to draw engine class

--- Arena/Scripts/FightArena/Arena.js
+++ Arena/Scripts/FightArena/Arena.js
@@ -1,30 +1,64 @@
 ﻿define("Arena", function () {
     function Arena(arenaDispaly) {
-        this.arena = arenaDispaly.getContext("2d");
-        this.size = { x: arenaDispaly.width, y: arenaDispaly.height };
         this.players = [];
         this.animatingTurnEnded = false;
 
+        var drawEngine = new DrawEngine(arenaDispaly, this);
         var self = this;
 
         self.turnEnded = function () {
             self.animatingTurnEnded = true;
             self.startTime = window.performance.now();
         };
+    }
+
+    Arena.prototype = {
+
+        addPlayer: function (players) {
+            for (var i = 0; i < players.length; i++) {
+                this.players.push(players[i]);
+            }
+        },
+
+        removePlayer: function (id) {
+            var player = this.players.filter(function (e) { return e.Id === id; });
+            if (player.length === 0)
+                return;
+
+            var playerIndex = this.players.indexOf(player);
+            this.players.splice(playerIndex, 1);
+        },
+
+        updatePlayer: function (player) {
+            var playerToUpdate = this.players.filter(function (e) { return e.Id === player.Id; });
+            if (playerToUpdate.length === 0)
+                return;
+
+            var playerIndex = this.players.indexOf(playerToUpdate[0]);
+            this.players[playerIndex] = player;
+        },
+    };
+
+    function DrawEngine(arenaDisplay, arena) {
+        this.arena = arena;
+        this.display = arenaDisplay.getContext("2d");
+        
+        var size = { width: arenaDisplay.width, height: arenaDisplay.height };
+        
+        var self = this;
 
         var update = function (time) {
-            self.arena.clearRect(0, 0, self.size.x, self.size.y);
+            self.display.clearRect(0, 0, size.width, size.height);
 
             var animationLength = 1000;
             var animationPoint = 0;
-            if (self.animatingTurnEnded) {
-                animationPoint = time - self.startTime;
-                self.animatingTurnEnded = (animationPoint < animationLength);
+            if (self.arena.animatingTurnEnded) {
+                animationPoint = time - self.arena.startTime;
+                self.arena.animatingTurnEnded = (animationPoint < animationLength);
             }
 
-
-            for (var i = 0; i < self.players.length; i++) {
-                var player = self.players[i].Token;
+            for (var i = 0; i < self.arena.players.length; i++) {
+                var player = self.arena.players[i].Token;
                 var movePath = player.MovePath.slice();
                 movePath.unshift(player.Position);
 
@@ -57,54 +91,26 @@
                         Y: startPoint.Y + currentPosition.Y,
                     };
 
-                paintPlayer(currentPosition, player.Size, movePath.slice(startIndex+1));
+                paintPlayer(currentPosition, player.Size, movePath.slice(startIndex + 1));
             }
 
             requestAnimationFrame(update);
         };
 
         var paintPlayer = function (position, size, movePath) {
-            self.arena.fillRect(position.X, position.Y, size.X, size.Y);
-
-            self.arena.strokeStyle = "#777";
-            self.arena.beginPath();
-            self.arena.moveTo(position.X, position.Y);
+            self.display.fillRect(position.X, position.Y, size.X, size.Y);
+            self.display.strokeStyle = "#777";
+            self.display.beginPath();
+            self.display.moveTo(position.X, position.Y);
             for (var j = 0; j < movePath.length; j++) {
                 var pathPoint = movePath[j];
-                self.arena.lineTo(pathPoint.X, pathPoint.Y);
+                self.display.lineTo(pathPoint.X, pathPoint.Y);
             }
-            self.arena.stroke();
+            self.display.stroke();
         }
 
         requestAnimationFrame(update);
     }
-
-    Arena.prototype = {
-
-        addPlayer: function (players) {
-            for (var i = 0; i < players.length; i++) {
-                this.players.push(players[i]);
-            }
-        },
-
-        removePlayer: function (id) {
-            var player = this.players.filter(function (e) { return e.Id === id; });
-            if (player.length === 0)
-                return;
-
-            var playerIndex = this.players.indexOf(player);
-            this.players.splice(playerIndex, 1);
-        },
-
-        updatePlayer: function (player) {
-            var playerToUpdate = this.players.filter(function (e) { return e.Id === player.Id; });
-            if (playerToUpdate.length === 0)
-                return;
-
-            var playerIndex = this.players.indexOf(playerToUpdate[0]);
-            this.players[playerIndex] = player;
-        },
-    };
 
     return Arena;
 });
