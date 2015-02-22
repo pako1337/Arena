99217d0566a99f954bcdd6101b00a5e1a676640b
jporwol <pako1337@gmail.com>
2015-02-22 16:00:03 +0100

    draw movement based on speed for animations

--- Arena/Scripts/FightArena/DrawEngine.js
+++ Arena/Scripts/FightArena/DrawEngine.js
@@ -4,13 +4,17 @@
         this.display = arenaDisplay.getContext("2d");
 
         var size = { width: arenaDisplay.width, height: arenaDisplay.height };
+        var lastTime = 0;
 
         var self = this;
 
         var update = function (time) {
+            if (lastTime === 0) lastTime = time;
             self.display.clearRect(0, 0, size.width, size.height);
 
             var animationLength = 1000;
+            var speed = 10;
+            var deltaT = time - lastTime;
             var animationPoint = 0;
             if (self.arena.animatingTurnEnded) {
                 animationPoint = time - self.arena.startTime;
@@ -19,41 +23,31 @@
 
             for (var i = 0; i < self.arena.players.length; i++) {
                 var player = self.arena.players[i].Token;
-                var movePath = player.MovePath.slice();
-                movePath.unshift(player.Position);
+                if (player.CurrentPosition === undefined) {
+                    player.CurrentPosition = player.Position;
+                    player.CurrentIndex = 0;
+                }
 
-                var segmentDuration = animationLength / movePath.length;
-                var startIndex = animationPoint / segmentDuration;
-                startIndex = parseInt(startIndex);
+                var currentPosition = player.CurrentPosition;
+                var endPoint = player.MovePath[player.CurrentIndex];
+                if (endPoint == undefined)
+                    endPoint = currentPosition;
 
-                startIndex = Math.min(startIndex, movePath.length - 1);
-                var endIndex = Math.min(startIndex + 1, movePath.length - 1);
+                var xDistance = endPoint.X - currentPosition.X;
+                var yDistance = endPoint.Y - currentPosition.Y;
 
-                var startPoint = movePath[startIndex];
-                var endPoint = movePath[endIndex];
+                var moveLength = Math.sqrt(xDistance * xDistance + yDistance * yDistance);
 
-                var segmentProgress = (animationPoint % segmentDuration) / segmentDuration;
+                var sin = yDistance / moveLength;
+                var cos = xDistance / moveLength;
 
-                var currentPosition =
-                    {
-                        X: (endPoint.X - startPoint.X) * segmentProgress,
-                        Y: (endPoint.Y - startPoint.Y) * segmentProgress
-                    };
+                currentPosition.X = currentPosition.X + speed * cos;
+                currentPosition.Y = currentPosition.Y + speed * sin;
 
-                if (!isFinite(currentPosition.X))
-                    currentPosition.X = 0;
-                if (!isFinite(currentPosition.Y))
-                    currentPosition.Y = 0;
-
-                currentPosition =
-                    {
-                        X: startPoint.X + currentPosition.X,
-                        Y: startPoint.Y + currentPosition.Y,
-                    };
-
-                paintPlayer(currentPosition, player.Size, movePath.slice(startIndex + 1));
+                paintPlayer(currentPosition, player.Size, player.MovePath.slice(player.CurrentIndex));
             }
 
+            last = time;
             requestAnimationFrame(update);
         };
 
